c438ff491d4bacebc2af468c4fdf5bea9f0c74ed
Madhupreetha98 <36624004+Madhupreetha98@users.noreply.github.com>
2018-02-20 12:01:04 +0530

    Create mysqi.py

---
+++ Pro Level/mysqi.py
@@ -0,0 +1,8 @@
+import mysql.connector
+connection = MySQLdb.connect (host = "192.168.1.2", user = "user", passwd = "password, db = "scripting_mysql")
+                              CREATE TABLE `address` (
+  `name_first` varchar(30) NOT NULL,
+  `name_last` varchar(30) NOT NULL);
+                              insert into `address` values('John','Smith);
+                                                           row = cursor.fetchone ()
+                                                           print row[0]
